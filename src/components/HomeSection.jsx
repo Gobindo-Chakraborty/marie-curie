aaa558c2b30a16e4f02e0c94b752591f7fc57b14
Gobindo Chakraborty <gobindochakraborty108@gmail.com>
2026-05-09 16:23:28 +0600

    profile image added

---
+++ src/components/HomeSection.jsx
@@ -0,0 +1,9 @@
+import profileImage from "../assets/images/marie-curie.jpg";
+
+export default function HomeSection() {
+  return (
+    <section>
+      <img src={profileImage} alt="Marie Curie Profile" />
+    </section>
+  );
+}
--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,10 +1,12 @@
 import Header from "../../components/Header";
+import HomeSection from "../../components/HomeSection";
 import WIP from "../../components/WIP";
 
 export default function HomePage() {
   return (
     <>
       <Header />
+      <HomeSection />
       <WIP />
     </>
   );
